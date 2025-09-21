4f5f5d5e440a571ea8f40bd8c094d4f54e41972b
Haev Grigoriy <tyuiu.haevgs@outlook.com>
2025-09-21 13:59:56 +0500

    Add project files.

---
+++ Tyuiu.HaevGS.Sprint1.Task0.V4.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tyuiu.HaevGS.Sprint1.Task0.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
