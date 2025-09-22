b3c5f67956a93f8c8cd9ebdbf692e52a1c9f05b6
Haev Grigoriy <tyuiu.haevgs@outlook.com>
2025-09-22 18:43:11 +0500

    Task2

---
+++ Tyuiu.HaevGS.Sprint1.Task2.V27.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json.Linq;
+using Tyuiu.HaevGS.Sprint1.Task2.V27.Lib;
+
+namespace Tyuiu.HaevGS.Sprint1.Task2.V27.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression() 
+        {
+            DataServiceTest ds = new DataServiceTest();
+            int x = 2;
+            var res = ds.CalculateSquarePerimetr(x);
+            Assert.AreEqual(4, res);
+        }
+    }
+}
---
+++ Tyuiu.HaevGS.Sprint1.Task2.V27/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.HaevGS.Sprint1.Task2.V27.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {   
+        DataService ds = new DataService();
+        int x;
+        x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(ds.CalculateSquarePerimetr(x));
+        Console.ReadLine();
+
+
+
+    }
+}
