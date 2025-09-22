1668db2b0ebe2e416fcb4cb7d96faf32a9a4cac1
Haev Grigoriy <tyuiu.haevgs@outlook.com>
2025-09-22 20:28:52 +0500

    Task3

---
+++ Tyuiu.HaevGS.Sprint1.Task3.V5.Test/DataSeviceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.HaevGS.Sprint1.Task3.V5.Lib;
+
+namespace Tyuiu.HaevGS.Sprint1.Task3.V5.Test
+{
+    [TestClass]
+    public sealed class DataSeviceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 3;
+            double wait = 6;
+            var res = ds.DistanceLength(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.HaevGS.Sprint1.Task3.V5/Program.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.HaevGS.Sprint1.Task3.V5.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        double mapScale = 120;
+        double distanceBetweenPoints = 3.5;
+
+        Console.WriteLine(mapScale);
+        Console.WriteLine(distanceBetweenPoints);
+
+        Console.Write(ds.DistanceLength(mapScale, distanceBetweenPoints));
+
+        Console.ReadKey(); 
+
+    }
+}
