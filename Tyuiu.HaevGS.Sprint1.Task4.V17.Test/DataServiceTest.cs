b87befbe36e1260b224016ac62f7a38e6fe910a6
Haev Grigoriy <tyuiu.haevgs@outlook.com>
2025-09-23 13:38:11 +0500

    Task4

---
+++ Tyuiu.HaevGS.Sprint1.Task4.V17.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.HaevGS.Sprint1.Task4.V17.Lib;
+
+namespace Tyuiu.HaevGS.Sprint1.Task4.V17.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 2;
+            double wait = 0.125;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.HaevGS.Sprint1.Task4.V17/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.HaevGS.Sprint1.Task4.V17.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        int x, y;
+        x = Convert.ToInt32(Console.ReadLine());
+        y = Convert.ToInt32(Console.ReadLine());
+
+        Console.ReadKey();
+    }
+}
