884d7b176271f356ec25c281bb91b085910d7e79
Haev Grigoriy <tyuiu.haevgs@outlook.com>
2025-09-23 13:52:04 +0500

    Task4

--- Tyuiu.HaevGS.Sprint1.Task4.V17.Lib/DataService.cs
+++ Tyuiu.HaevGS.Sprint1.Task4.V17.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.HaevGS.Sprint1.Task4.V17.Lib
         public double Calculate(double x, double y)
         {
             var res = 1/(Math.Sqrt(x * 1 - y * 5));
-            return res;
+            return Math.Round(res, 3);
         }
     }
 }
