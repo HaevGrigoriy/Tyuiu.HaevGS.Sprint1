7641aa5516b07c0473d18deb9e2627498705a943
Haev Grigoriy <tyuiu.haevgs@outlook.com>
2025-09-24 20:04:29 +0500

    Task6

--- Tyuiu.HaevGS.Sprint1.Task6V13/Program.cs
+++ Tyuiu.HaevGS.Sprint1.Task6V13/Program.cs
@@ -4,6 +4,7 @@ internal class Program
     private static void Main(string[] args)
     {
         DataService ds = new DataService();
+
         string str = Console.ReadLine();
         Console.WriteLine("{0} по алфавиту? {1}", str, ds.CheckWordsAlphabet(str));
         Console.ReadKey();
